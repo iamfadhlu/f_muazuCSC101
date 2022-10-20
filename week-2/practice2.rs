af99b1556272e05588ec6a845e3d39f0f63fdd8f
iamfadhlu <fromeo016@gmail.com>
2022-10-20 18:12:52 +0100

    (My First Commit)

---
+++ week-2/practice2.rs
@@ -0,0 +1,7 @@
+fn main() {
+	println!();
+	println!("Hallo");
+	println!("How {} you doing?", "are" );
+	println!("My name is {}. I am a student of Computer Science Stream 1","Fadhilullahi Muazu" );
+	
+}
